a6aa0b71c2d8d138580856e0eb3fd7ccffac46ed
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2020-12-24 15:17:13 +0200

    C# Basics

---
+++ Conditional Statements - Lab/06.AreaOfFigures/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _06.AreaOfFigures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string typeOfFigure = Console.ReadLine();
+
+            if (typeOfFigure == "square")
+            {
+                double a = double.Parse(Console.ReadLine());
+                Console.WriteLine(typeOfFigure);
+                Console.WriteLine($"{a * a:F3}");
+            }
+            else if (typeOfFigure == "rectangle")
+            {
+                double a = double.Parse(Console.ReadLine());
+                double b = double.Parse(Console.ReadLine());
+                Console.WriteLine(typeOfFigure);
+                Console.WriteLine($"{a * b:F3}");
+            }
+            else if (typeOfFigure == "circle")
+            {
+                double a = double.Parse(Console.ReadLine());
+                Console.WriteLine(typeOfFigure);
+                Console.WriteLine($"{Math.PI * a * a:F3}");
+            }
+            else if (typeOfFigure == "triangle")
+            {
+                double a = double.Parse(Console.ReadLine());
+                double b = double.Parse(Console.ReadLine());
+                Console.WriteLine(typeOfFigure);
+                Console.WriteLine($"{a / 2 * b:F3}");
+            }
+        }
+    }
+}
---
+++ Conditional Statements - Lab/06.PasswordGuess/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _06.PasswordGuess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pass = Console.ReadLine();
+
+            if (pass == "s3cr3t!P@ssw0rd")
+
+            {
+                Console.WriteLine("Welcome");
+            }
+
+            else if(pass != "s3cr3t!P@ssw0rd")
+
+            {
+                Console.WriteLine("Wrong password!");
+            }
+        }
+    }
+}
