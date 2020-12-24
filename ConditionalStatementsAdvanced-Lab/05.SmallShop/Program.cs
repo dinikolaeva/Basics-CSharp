fe90a4754ba9aebe8e2e7212bcf88b5ba974191d
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2020-12-24 14:57:33 +0200

    Conditional Statements Advanced - Lab

---
+++ ConditionalStatementsAdvanced-Lab/05.SmallShop/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace _05.SmallShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            string town = Console.ReadLine();
+            double count = double.Parse(Console.ReadLine());
+
+            switch (town)
+            {
+                case "Sofia":
+                    if (product == "coffee")
+                    {
+                        double allPrice = count * 0.50;
+                        Console.WriteLine(allPrice);
+                    }
+                    else if (product == "water")
+                    {
+                        double allPrice = count * 0.80;
+                        Console.WriteLine(allPrice);
+                    }
+                    else if (product == "beer")
+                    {
+                        double allPrice = count * 1.20;
+                        Console.WriteLine(allPrice);
+                    }
+                    else if (product == "sweets")
+                    {
+                        double allPrice = count * 1.45;
+                        Console.WriteLine(allPrice);
+                    }
+                    else if (product == "peanuts")
+                    {
+                        double allPrice = count * 1.60;
+                        Console.WriteLine(allPrice);
+                    }
+                    break;
+                case "Plovdiv":
+                    if (product == "coffee")
+                    {
+                        double allPrice = count * 0.40;
+                        Console.WriteLine(allPrice);
+                    }
+                    else if (product == "water")
+                    {
+                        double allPrice = count * 0.70;
+                        Console.WriteLine(allPrice);
+                    }
+                    else if (product == "beer")
+                    {
+                        double allPrice = count * 1.15;
+                        Console.WriteLine(allPrice);
+                    }
+                    else if (product == "sweets")
+                    {
+                        double allPrice = count * 1.30;
+                        Console.WriteLine(allPrice);
+                    }
+                    else if (product == "peanuts")
+                    {
+                        double allPrice = count * 1.50;
+                        Console.WriteLine(allPrice);
+                    }
+                    break;
+                case "Varna":
+                    if (product == "coffee")
+                    {
+                        double allPrice = count * 0.45;
+                        Console.WriteLine(allPrice);
+                    }
+                    else if (product == "water")
+                    {
+                        double allPrice = count * 0.70;
+                        Console.WriteLine(allPrice);
+                    }
+                    else if (product == "beer")
+                    {
+                        double allPrice = count * 1.10;
+                        Console.WriteLine(allPrice);
+                    }
+                    else if (product == "sweets")
+                    {
+                        double allPrice = count * 1.35;
+                        Console.WriteLine(allPrice);
+                    }
+                    else if (product == "peanuts")
+                    {
+                        double allPrice = count * 1.55;
+                        Console.WriteLine(allPrice);
+                    }
+                    break;
+            }
+
+ 
+        }
+    }
+}
---
+++ ConditionalStatementsAdvanced-Lab/11.FruitShop/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.IO;
+
+namespace _11.FruitShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fruit = Console.ReadLine();
+            string day = Console.ReadLine();
+            double count = double.Parse(Console.ReadLine());
+
+            double price = 0;
+
+            if (day == "Monday" || day == "Tuesday" || day == "Wednesday" || day == "Thursday" || day == "Friday")
+            {
+                if (fruit == "banana")
+                {
+                    price = count * 2.50;
+                    Console.WriteLine($"{price:f2}");
+                }
+                else if (fruit == "apple")
+                {
+                    price = count * 1.20;
+                    Console.WriteLine($"{price:f2}");
+                }
+                else if (fruit == "orange")
+                {
+                    price = count * 0.85;
+                    Console.WriteLine($"{price:f2}");
+                }
+                else if (fruit == "grapefruit")
+                {
+                    price = count * 1.45;
+                    Console.WriteLine($"{price:f2}");
+                }
+                else if (fruit == "kiwi")
+                {
+                    price = count * 2.70;
+                    Console.WriteLine($"{price:f2}");
+                }
+                else if (fruit == "pineapple")
+                {
+                    price = count * 5.50;
+                    Console.WriteLine($"{price:f2}");
+                }
+                else if (fruit == "grapes")
+                {
+                    price = count * 3.85;
+                    Console.WriteLine($"{price:f2}");
+                }
+                else
+                {
+                    Console.WriteLine("error");
+                }
+            }
+            else if (day == "Saturday" || day == "Sunday")
+            {
+                if (fruit == "banana")
+                {
+                    price = count * 2.70;
+                    Console.WriteLine($"{price:f2}");
+                }
+                else if (fruit == "apple")
+                {
+                    price = count * 1.25;
+                    Console.WriteLine($"{price:f2}");
+                }
+                else if (fruit == "orange")
+                {
+                    price = count * 0.90;
+                    Console.WriteLine($"{price:f2}");
+                }
+                else if (fruit == "grapefruit")
+                {
+                    price = count * 1.60;
+                    Console.WriteLine($"{price:f2}");
+                }
+                else if (fruit == "kiwi")
+                {
+                    price = count * 3.00;
+                    Console.WriteLine($"{price:f2}");
+                }
+                else if (fruit == "pineapple")
+                {
+                    price = count * 5.60;
+                    Console.WriteLine($"{price:f2}");
+                }
+                else if (fruit == "grapes")
+                {
+                    price = count * 4.20;
+                    Console.WriteLine($"{price:f2}");
+                }
+                else
+                {
+                    Console.WriteLine("error");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("error");
+            }
+        }
+    }
+}
