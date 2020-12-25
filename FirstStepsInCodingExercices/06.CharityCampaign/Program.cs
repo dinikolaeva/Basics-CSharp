0694c4598ed310677051c22e9fc9ec52917305f2
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2020-12-25 16:12:22 +0200

    C# Basics

---
+++ FirstStepsInCodingExercices/06.CharityCampaign/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _06.CharityCampaign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double cakePrice = 45.0;
+            double wafflePrice = 5.8;
+            double pancakePrice = 3.2;
+
+            int dayOfCampaign = int.Parse(Console.ReadLine());
+            int countOfCoocers = int.Parse(Console.ReadLine());
+            int cakes = int.Parse(Console.ReadLine());
+            int waffles = int.Parse(Console.ReadLine());
+            int pancakes = int.Parse(Console.ReadLine());
+
+            double sumFromCakes = cakes * cakePrice;
+            double sumFromWaffles = waffles * wafflePrice;
+            double sumFromPancakes = pancakes * pancakePrice;
+            double totalSumPerDay = (sumFromCakes + sumFromWaffles + sumFromPancakes) * countOfCoocers;
+            double allCampaignSum = totalSumPerDay * dayOfCampaign;
+            double totalSum = allCampaignSum - (allCampaignSum / 8);
+
+
+            Console.WriteLine(totalSum);
+
+        }
+    }
+}
