ff61815cf5f70aebab146503137853e95a5636df
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2020-12-25 16:14:43 +0200

    C# Basics

---
+++ Greetings/Greetings2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Greetings2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            
+            Console.WriteLine("Hello," + name + "!");
+        }
+    }
+}
