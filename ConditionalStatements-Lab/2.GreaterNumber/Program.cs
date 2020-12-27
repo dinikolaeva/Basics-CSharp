a761e5e99987aadf8cd8dbd67601e9f3f962993c
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2020-12-27 16:24:44 +0200

    Lab

---
+++ ConditionalStatements-Lab/2.GreaterNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _02.GreaterNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            if (number >= secondNumber)
+            {
+                Console.WriteLine(number);
+            }
+            else if (secondNumber >=number)
+            {
+                Console.WriteLine(secondNumber);
+            }
+        }
+    }
+}
