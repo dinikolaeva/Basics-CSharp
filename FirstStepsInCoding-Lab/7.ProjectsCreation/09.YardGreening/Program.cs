370bcf369e777c35ad3046a7c5373f695cba550c
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2020-12-27 16:04:44 +0200

    Lab

---
+++ FirstStepsInCoding-Lab/7.ProjectsCreation/09.YardGreening/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _09.YardGreening
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double yard = double.Parse(Console.ReadLine());
+            double price = yard * 7.61;
+            double discount = price * 0.18;
+            double finalPrice = price - discount;
+
+            Console.WriteLine($"The final price is: {finalPrice} lv.");
+            Console.WriteLine($"The final discount is: {discount} lv.");
+        }
+    }
+}
---
+++ FirstStepsInCoding-Lab/8.PetShop/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection.Metadata;
+
+namespace PetShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dogs = int.Parse(Console.ReadLine());
+            int oters = int.Parse(Console.ReadLine());
+            double foodDogs = dogs * 2.50;
+            double foodOters = oters * 4.00;
+            double finalAmaunt = foodDogs + foodOters;
+
+            Console.WriteLine($"{finalAmaunt} lv.");
+        }
+    }
+}
