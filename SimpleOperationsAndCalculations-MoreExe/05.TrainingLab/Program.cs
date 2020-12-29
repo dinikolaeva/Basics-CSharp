9304952d29cde6b6aa314b34b0fdcf34453e45f1
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2020-12-29 23:19:08 +0200

    Exercise

---
+++ SimpleOperationsAndCalculations-MoreExe/05.TrainingLab/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Transactions;
+
+namespace _005.TrainingLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double leightW = double.Parse(Console.ReadLine());
+            double weightH = double.Parse(Console.ReadLine());
+
+            double leightWSm = leightW * 100;
+            double weightHSm = weightH * 100;
+
+            double rows = Math.Floor(leightWSm / 120);
+            double bearew = Math.Floor((weightHSm - 100) / 70);
+            double seats = (rows * bearew) - 3;
+
+            Console.WriteLine(seats);
+
+
+
+        }
+    }
+}
---
+++ SimpleOperationsAndCalculations-MoreExe/06.Fishland/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _006.Fishland
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Паламуд – 60% по-скъп от скумрията
+            //Сафрид – 80 % по - скъп от цацата
+            //Миди – 7.50 лв.за килограм
+
+
+            double mackerelPrice = double.Parse(Console.ReadLine());
+            double toyPrice = double.Parse(Console.ReadLine());
+            double bonitoKg = double.Parse(Console.ReadLine());
+            double horseMackerelKg = double.Parse(Console.ReadLine());
+            double musselsKg = double.Parse(Console.ReadLine());
+
+            double bonitoPrice = mackerelPrice + (mackerelPrice * 0.60);
+            double horseMackerelPrice = toyPrice + (toyPrice * 0.80);
+            double musselsPrice = 7.50;
+            double sumBonito = bonitoPrice * bonitoKg;
+            double sumHorseMackerel = horseMackerelKg * horseMackerelPrice;
+            double sumMussels = musselsKg * musselsPrice;
+       
+            double total = sumBonito + sumHorseMackerel + sumMussels;
+
+            Console.WriteLine($"{total:F2}");
+
+        }
+    }
+}
