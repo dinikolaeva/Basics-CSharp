632515851b116f089f018b61fe42053cd5c8add7
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2020-12-29 22:52:05 +0200

    Exercise

---
+++ FirstStepsInCoding-Exe/1.USDToBGN/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MoneyConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double courseUsdToBgn = 1.79549;
+            double usd = double.Parse(Console.ReadLine());
+            double bgn = usd * courseUsdToBgn;
+
+            Console.WriteLine($"{bgn:F2}");
+
+
+        }
+    }
+}
---
+++ FirstStepsInCoding-Exe/5.BirthdayParty/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _05.BirthdayParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double rentForHall = double.Parse(Console.ReadLine());
+            double cake = rentForHall * 0.20;
+            double drink = cake - (cake * 0.45);
+            double animator = rentForHall / 3;
+            double totalSum = rentForHall + cake + drink + animator;
+
+            Console.WriteLine(totalSum);
+
+        }
+    }
+}
