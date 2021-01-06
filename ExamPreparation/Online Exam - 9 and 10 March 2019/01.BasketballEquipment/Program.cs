c19af2d6b5003de2c9ff01ce96ebab65cd58d0b4
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2021-01-07 01:09:24 +0200

    Exams

---
+++ ExamPreparation/Online Exam - 9 and 10 March 2019/01.BasketballEquipment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _01.BasketballEquipment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double annualFee = double.Parse(Console.ReadLine());
+
+            double sneakersPrice = annualFee - (annualFee * 0.40);
+            double equpmentPrice = sneakersPrice - (sneakersPrice * 0.20);
+            double basketball = equpmentPrice / 4;
+            double accessoires = basketball / 5;
+
+            double totalSum = annualFee + sneakersPrice + equpmentPrice + basketball + accessoires;
+
+            Console.WriteLine($"{totalSum:f2}");
+        }
+    }
+}
---
+++ ExamPreparation/Online Exam - 9 and 10 March 2019/demo/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace _06.HighJump
+{
+    class Program
+        {
+            static void Main(string[] args)
+            {
+                int wishedHeight = int.Parse(Console.ReadLine());
+
+                int jumpsSUCSESSCounter = 0; // връщам опитите до 0!
+                int jumpsWithoutSUCSESSCounter = 0;
+                int letvaHeight = wishedHeight - 30;
+
+                int heightChageSum = wishedHeight;
+
+                int jumpAgain = 0;
+
+
+                while (true) //?!? heightChageSum >= letvaHeight - целта е първо да прескаме летвата, докато стигнем до желаното ниво!
+                {
+                    jumpAgain = int.Parse(Console.ReadLine());
+
+                    jumpsSUCSESSCounter++;
+
+                    //if (letvaHeight == heightChageSum) // първо трябва да сме сигурни, че е прескочена летвата успешно
+                    //{
+                    //    break;
+                    //}
+
+                    if (jumpAgain > letvaHeight) // тук е критичен момент в програмата - уверяваме се дали има успешен скок и ако е успешен дали нивото на летвата е стигнало желаната височина
+                    {
+
+                        if (letvaHeight >= heightChageSum) //jumpAgain >= heightChageSum
+                        {
+                            break;
+                            //letvaHeight += 5; // това не е нужно
+                            //jumpsWithoutSUCSESSCounter = 0;
+                            //jumpsSUCSESSCounter++;
+                            //continue;
+                        }
+
+                        letvaHeight += 5;
+                        jumpsWithoutSUCSESSCounter = 0;
+
+                    }
+                    else if (jumpAgain <= letvaHeight)
+                    {
+                        jumpsWithoutSUCSESSCounter++;
+
+                    }
+
+
+                    if (jumpsWithoutSUCSESSCounter == 3)
+                    {
+                        Console.WriteLine($"Tihomir failed at {letvaHeight}cm after {jumpsSUCSESSCounter} jumps.");
+                        return;
+
+                    }
+
+                }
+
+                Console.WriteLine($"Tihomir succeeded, he jumped over {letvaHeight}cm after {jumpsSUCSESSCounter} jumps.");
+
+            }
+        }
+    }
+
