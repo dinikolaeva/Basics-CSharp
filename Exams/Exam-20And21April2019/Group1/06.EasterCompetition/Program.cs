709bcebe06125edd044a829b88395b9ab2504c14
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2021-01-08 15:35:46 +0200

    Exams

---
+++ Exams/Exam-20And21April2019/Group1/06.EasterCompetition/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _06.EasterCompetition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfEasterBreads = int.Parse(Console.ReadLine());
+
+            int sumOfGrades = 0;
+            int maxChefGrades = int.MinValue;
+            string topChef = "";
+
+            for (int newParticipiant = 0; newParticipiant < countOfEasterBreads; newParticipiant++)
+            {
+
+                string nameOfBaker = Console.ReadLine();
+                string gradeForEasterBread = Console.ReadLine();
+                sumOfGrades = 0;
+
+                while (gradeForEasterBread != "Stop")
+                {
+                    int gradeParse = int.Parse(gradeForEasterBread);
+
+                    sumOfGrades += gradeParse;
+
+                    gradeForEasterBread = Console.ReadLine();
+                }
+
+                Console.WriteLine($"{nameOfBaker} has {sumOfGrades} points.");
+
+                if (sumOfGrades > maxChefGrades)
+                {
+                    maxChefGrades = sumOfGrades;
+                    topChef = nameOfBaker;
+                    Console.WriteLine($"{topChef} is the new number 1!");
+                }
+
+            }
+
+            Console.WriteLine($"{topChef} won competition with {maxChefGrades} points!");
+
+
+        }
+    }
+}
---
+++ Exams/Exam-20And21April2019/Group2/04.EasterShop/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _4.EasterShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startCountOfEggs = int.Parse(Console.ReadLine());
+            string command = Console.ReadLine();
+
+            int countOfSoldEggs = 0;
+
+            while (command != "Close")
+            {
+                int countOfEggsBuyOrFill = int.Parse(Console.ReadLine());
+
+                if (command == "Buy")
+                {
+
+                    if (countOfEggsBuyOrFill > startCountOfEggs)
+                    {
+                        Console.WriteLine("Not enough eggs in store!");
+                        Console.WriteLine($"You can buy only {startCountOfEggs}.");
+                        break;
+                    }
+
+                    startCountOfEggs -= countOfEggsBuyOrFill;
+                    countOfSoldEggs += countOfEggsBuyOrFill;
+                }
+                else
+                {
+                    startCountOfEggs += countOfEggsBuyOrFill;
+                }
+
+                command = Console.ReadLine();                
+            }
+
+            if (command == "Close")
+            {
+                Console.WriteLine("Store is closed!");
+                Console.WriteLine($"{countOfSoldEggs} eggs sold.");
+            }
+         
+        }
+    }
+}
