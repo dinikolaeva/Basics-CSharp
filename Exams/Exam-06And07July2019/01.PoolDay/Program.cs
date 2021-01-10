e8b174256b72b2e4b57925d66ab1093cf49c9e74
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2021-01-11 01:07:36 +0200

    Exams

---
+++ Exams/Exam-06And07July2019/01.PoolDay/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _01.PoolDay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfPeople = int.Parse(Console.ReadLine());
+            double entranceFee = double.Parse(Console.ReadLine());
+            double deckChairPrice = double.Parse(Console.ReadLine());
+            double umbrellaPrice = double.Parse(Console.ReadLine());
+
+            double sumOfEntranceFee = countOfPeople * entranceFee;
+            double percentOfDeckChairs = Math.Ceiling(countOfPeople * 0.75);
+            double percentOfUmbrellas = Math.Ceiling(countOfPeople * 0.50);
+            double sumOfDeckChair = percentOfDeckChairs * deckChairPrice;
+            double sumOfUmbrellas = percentOfUmbrellas * umbrellaPrice;
+
+            double totalSum = sumOfEntranceFee + sumOfDeckChair + sumOfUmbrellas;
+
+            Console.WriteLine($"{totalSum:F2} lv.");
+        }
+    }
+}
---
+++ Exams/Exam-06And07July2019/02.FamilyTrip/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _02.FamilyTrip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int nights = int.Parse(Console.ReadLine());
+            double nightPrice = double.Parse(Console.ReadLine());
+            int percentExtraCosts = int.Parse(Console.ReadLine());
+
+            if (nights > 7)
+            {
+                nightPrice -= nightPrice * 0.05;
+            }
+
+            double sumOfAllNights = nights * nightPrice;
+
+            double percentExtra = budget * (percentExtraCosts * 1.0/ 100);
+            double allSum = sumOfAllNights + percentExtra;
+
+            if (budget>=allSum)
+            {
+                double leftSum = budget - allSum;
+                Console.WriteLine($"Ivanovi will be left with {leftSum:f2} leva after vacation.");
+            }
+            else
+            {
+                double neededSum = allSum - budget;
+                Console.WriteLine($"{neededSum:f2} leva needed.");
+            }
+        }
+    }
+}
