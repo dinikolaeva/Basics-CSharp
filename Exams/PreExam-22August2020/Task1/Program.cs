da20899c2773588f5a219a20d8990b9ca4356f6d
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2021-01-14 00:40:56 +0200

    Exams

---
+++ Exams/PreExam-22August2020/Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Contest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double dollarInLeva = 1.57;
+
+            double processorPrice = double.Parse(Console.ReadLine());
+            double videoCardPrice = double.Parse(Console.ReadLine());
+            double oneRamPrice = double.Parse(Console.ReadLine());
+            int countOfRam = int.Parse(Console.ReadLine());
+            double discountPercent = double.Parse(Console.ReadLine());
+
+            double processorPriceInLeva = processorPrice * dollarInLeva;
+            double videoCardPriceInLeva = videoCardPrice * dollarInLeva;
+            double oneRamPriceInLeva = oneRamPrice * dollarInLeva;
+            double allPriceRam = oneRamPriceInLeva * countOfRam;
+            double discountPriceOfProcessor = processorPriceInLeva - (processorPriceInLeva * discountPercent);
+            double discountPriceOfVideoCard = videoCardPriceInLeva - (videoCardPriceInLeva * discountPercent);
+
+            double totalSum = discountPriceOfProcessor + discountPriceOfVideoCard + allPriceRam;
+
+            Console.WriteLine($"Money needed - {totalSum:f2} leva.");
+        }
+    }
+}
